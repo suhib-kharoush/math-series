d2b667179d43b3c8eacbe0a83d321e3377d9fe4c
suhib-kharoush <suhaib_kharwash@outlook.com>
2021-06-11 20:45:39 +0300

    finished

--- math_series/series.py
+++ math_series/series.py
@@ -8,11 +8,13 @@ def fibonacci(n):
 
 
 def lucas(n):
-    # if n == 0:
-    #     return 2
-    # elif n==1:
-    #     return 1
-    # return lucas(n-1) + lucas(n-2) 
+    """
+    if n == 0:
+        return 2
+    elif n==1:
+        return 1
+    return lucas(n-1) + lucas(n-2) 
+    """
     return sum_series(n, 2, 1)
 
 
